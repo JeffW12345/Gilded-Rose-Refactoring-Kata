1a184ff224a795d8909afbb3a4d20bbed1b6a771
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2022-10-11 09:02:24 +0100

    Fixed  bug

--- src/main/java/com/gildedrose/calculatequality/CalcQualityStandard.java
+++ src/main/java/com/gildedrose/calculatequality/CalcQualityStandard.java
@@ -7,7 +7,7 @@ public class CalcQualityStandard implements CalcQuality {
 
     @Override
     public void setQualityScore(Item item) {
-        if (item.sellIn >= 0) {
+        if (item.sellIn > 0) {
             item.quality--;}
         else {item.quality -= 2;}
     }
